3602b6fe0cd4f1de8316fdee3b34fa6eb325e2e0
CScori <amilanotte1366@gmail.com>
2019-11-01 13:12:02 -0500

    projects fnc

--- data/Routes/project-router.js
+++ data/Routes/project-router.js
@@ -10,6 +10,64 @@ p_router.get('/', (req, res) => {
         res.status(500).json({ error: `The project could not be retrieved: ${err}`})
     })
 })
+p_router.get('/:id', validatePId, (req,res) => {
+    Projects.get(req.params.id)
+    .then(project => {
+        res.status(200).json(projects)
+    })
+    .catch(err => {
+        res.status(500).json({error: `Server could not get data: ${err}`})
+    })
+})
+p_router.get('/:id/actions', validProject, (req, res) => {
+    Projects.getProjectActions(req.params,id)
+    .then(actions => {
+        res.status(200).json(actions)
+    })
+    .catch(err => {
+        res.status(500).json({error: `Server error: ${err}`})
+    })
+})
+p_router.post('/', validProject, (req,res) => {
+    Projects.insert(req.body)
+    .then(newP => {
+        res.status(201).json(newP)
+    })
+    .catch(err => {
+        res.status(500).json({error: `Server error adding : ${err}`})
+    })
+})
+p_router.delete('/:id', validatePId, (req, res) => {
+    const pid = req.params.id
+    Projects.get(id)
+    .then(project => {
+        Projects.remove(pid)
+        .then(presp => {
+            if (presp === 1) {
+                res.status(204).json({message: 'deleted p'})
+            } else {
+                res.status(500).json({message: 'not deleted p '})
+            }
+        })
+    })
+    .catch(err => {
+        res.status(500).json({error: `Server error: ${err}`})
+    })
+})
+p_router.put('/:id', validatePId, (req, res) => {
+    const id = req.params.id
+    Projects.update(id, req.body)
+    .then(updateP => {
+        if (updateP) {
+            res.status(200).json(updateP)
+        } else {
+            res.status(500).json({error: 'Project not updated'})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({error: `Server error: ${err}`})
+    })
+})
 
 
 function validatePId(req, res, next) {
