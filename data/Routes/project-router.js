7910f5351841f8bffbd72892d42bb24b3743e7f6
CScori <amilanotte1366@gmail.com>
2019-11-01 12:46:38 -0500

    projects get

--- data/Routes/project-router.js
+++ data/Routes/project-router.js
@@ -1,6 +1,14 @@
 const p_router = require('express').Router()
 const Projects = require('../helpers/actionModel.js')
 
-
+p_router.get('/', (req, res) => {
+    Projects.get()
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+    .catch(err => {
+        res.status(500).json({ error: `The project could not be retrieved: ${err}`})
+    })
+})
 
 module.exports = p_router
