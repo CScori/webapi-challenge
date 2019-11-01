9c3d9db0a4e366a8770ee6ca8523c38dbfe5fa09
CScori <amilanotte1366@gmail.com>
2019-11-01 12:50:02 -0500

    reuse middleware for project

--- data/Routes/project-router.js
+++ data/Routes/project-router.js
@@ -11,4 +11,31 @@ p_router.get('/', (req, res) => {
     })
 })
 
+
+function validatePId(req, res, next) {
+    const id = req.params.id
+    Projects.get(id)
+    .then(project => {
+        if(project) {
+            next();
+        } else {
+            res.status(404).json({Messgae: "invalid project id"})
+        }
+    }).catch (error =>
+        res.status(500).json({error: `Server error: ${error}`})
+    )
+};
+
+function validProject(req, res, next) {
+    if (req.body) {
+        if (req.body.name && req.body.description) {
+            next ();
+        } else {
+            res.status(400).json({ message: "missing required name or description field"  })
+        }
+    } else {
+        res.status(400).json({ message: "missing project data" })
+    }
+    
+};
 module.exports = p_router
