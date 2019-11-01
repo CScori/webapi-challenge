e334f2b592e06b1a0511be15926602281764e999
CScori <amilanotte1366@gmail.com>
2019-11-01 10:37:10 -0500

    adds initial get in sub route

--- data/action-router.js
+++ data/action-router.js
@@ -1,7 +1,16 @@
 const router = require('express').Router()
 const Actions = require('./helpers/actionModel')
 
+router.get('/', (req, res) => {
 
+    Actions.get()
+    .then(actions => {
+        res.status(200).json(actions)
+    })
+    .catch(err => {
+        res.status(500).json({ error: `The action information could not be retrieved error: ${error}.` })
+    })
+})
 
 
 module.exports = router
